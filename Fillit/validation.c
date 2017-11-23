155091d0a1164b339a511f2cafd41060db46aefc
pavelrosh <16684328+pavelrosh@users.noreply.github.com>
2017-11-23 19:11:57 +0200

    недопиленная валидашка

--- Fillit/validation.c
+++ Fillit/validation.c
@@ -11,44 +11,109 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-int		size_checker(char *s)
+int		newline_check(char *s)
 {
+	int count;
 	int i;
-	int n;
-	int line_len;
 
-	n = 1;
+	count = 1;
 	i = 0;
-	line_len = 0;
-	while (i < 4)
+
+	while (count >= 1)
 	{
-		while (*s != '\n' && *s != '\0')
+		if ((s[i] != '.' || s[i] != '#') && s[i + 4] != '\n')
+			return (0);
+		if (count == 4)
 		{
-			if ((*s == '.' || *s == '#') && line_len < 4)
-			{
-				s++;
-				line_len++;
-			}
+			if (s[i + 5] == '\n' && (s[i + 6] == '.' || s[i + 6] == '#'))
+				return (1);
+			else if (s[i + 5] == '\0')
+				return (1);
 			else
 				return (0);
 		}
-		if (line_len == 4)
-		{
-			line_len = 0;
-			s++;
-		}
-		else
-			return (0);
-		i++;
+		count++;
+		i += 5;
 	}
 	return (1);
 }
 
-#include <stdio.h>
+int		tetr_check(char **str, int j, int i)
+{
+	int conn;
+
+	conn = 0;
+	if (str[j][i + 1] == '#')
+		conn++;
+	if (str[j][i - 1] == '#')
+		conn++;
+	if (str[j][i + 5] == '#')
+		conn++;
+	if (str[j][i - 5] == '#')
+		conn++;
+return (conn);
+}
+
+int		find_sharp(char *buf)
+{
+	char **str;
+	int s;
+	int l;
+	int conn;
+
+	s = 0;
+	l = 0;
+	conn = 0;
+	if ((str = ft_strsplit(buf, '\n')) != NULL)
+	{
+		while (l < 4)
+		{
+			while (s < 4)
+			{
+				if (str[l][s] != '#')
+					s++;
+				else
+					conn += tetr_check(str, l, s);
+			}
+			s = 0;
+			l++;
+		}
+		if (conn == 6 || conn == 8)
+			return (1);
+	}
+	return (0);
+}
 
 int		main()
 {
-	printf("%d\n", size_checker("....\n..##\n..##\n...."));
+	printf("result: %d\n", find_sharp("####\n....\n....\n....\n"));
 	return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
